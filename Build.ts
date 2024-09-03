83bd66808c4042f9dd6592b6331e1066806f9265
TSingleton <togetheryear@gmail.com>
2024-09-03 11:49:48 +0800

    Update:Build

--- Build.ts
+++ Build.ts
@@ -6,46 +6,57 @@ import esbuild from 'rollup-plugin-esbuild';
 import json from '@rollup/plugin-json';
 import alias from '@rollup/plugin-alias';
 import { join } from 'path';
+import * as fs from 'fs';
 
 const boundEnv = process.argv.slice(-1)[0];
 
-function ConfigFactory() {
-    const options: RollupOptions = {
-        input: 'Src/index.ts',
-        output: {
-            file: 'Build/bundle.js',
-            format: 'cjs',
-            sourcemap: false
-        },
-        plugins: [
-            nodeResolve(),
-            commonjs(),
-            json(),
-            typescript({ sourceMap: false, module: 'esnext' }),
-            esbuild({ minify: true, target: 'node20' }),
-            alias({
-                entries: [{ find: '@', replacement: join(__dirname, 'Src') }]
+const name = '[Build.ts]';
+
+const extra = ['hmc-win32'];
+
+function Bundle() {
+    return new Promise((resolve, reject) => {
+        const options: RollupOptions = {
+            input: 'Src/index.ts',
+            output: {
+                file: 'Build/bundle.js',
+                format: 'cjs',
+                sourcemap: false
+            },
+            plugins: [
+                nodeResolve(),
+                commonjs(),
+                json(),
+                typescript({ sourceMap: false, module: 'esnext' }),
+                esbuild({ minify: true, target: 'node20' }),
+                alias({
+                    entries: [{ find: '@', replacement: join(__dirname, 'Src') }]
+                })
+            ],
+            external: extra
+        };
+        console.log(name, 'Start');
+        rollup(options)
+            .then(async (build) => {
+                await build.write(options.output as OutputOptions);
+                resolve('Finish');
             })
-        ],
-        external: ['hmc-win32']
-    };
-    return options;
+            .catch((error) => {
+                console.log(error);
+                console.error(name, 'Error');
+                reject('Error');
+            });
+    });
 }
 
-const opts = ConfigFactory();
-
-const TAG = '[Build.ts]';
-
-if (boundEnv === 'development') {
-} else {
-    console.log(TAG, 'Start');
-    rollup(opts)
-        .then((build) => {
-            console.log(TAG, 'Success');
-            build.write(opts.output as OutputOptions);
-        })
-        .catch((error) => {
-            console.log(error);
-            console.error(TAG, 'Error');
-        });
+function AppendResource() {
+    const end = ['ts', 'json', 'md'];
+    for (let e of extra) {
+        fs.cpSync(join(__dirname, `node_modules/${e}`), join(__dirname, `Build/node_modules/${e}`), { recursive: true, filter: (e) => end.indexOf(e.split('.').slice(-1)[0]) === -1 });
+    }
+    console.log(name, 'Success');
 }
+
+Bundle().then(() => {
+    AppendResource();
+});
